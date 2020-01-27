d5ab302a975dc2507e9c13ac042913a958ed938b
Saltibarsciai <rakunas.dominykas@gmail.com>
2020-01-27 03:22:29 +0200

    remove Client from arguments, can't use with service container

--- server/app/Services/DataFetchService.php
+++ server/app/Services/DataFetchService.php
@@ -11,17 +11,11 @@ use GuzzleHttp\Exception\ServerException;
 
 class DataFetchService implements FetchableMeteoData
 {
-    private $client;
-
-    public function __construct(Client $client)
-    {
-        $this->client = $client;
-    }
-
     public function getMeteoResponse(string $city): string
     {
+        $client = new Client();
         try {
-            $response = $this->client->get("https://api.meteo.lt/v1/places/$city/forecasts/long-term")->getBody();
+            $response = $client->get("https://api.meteo.lt/v1/places/$city/forecasts/long-term")->getBody();
         } catch (Exception $e) {
             throw new \Exception($e->getCode());
         }
